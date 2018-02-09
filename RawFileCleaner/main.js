b1fd230c3492874c044a65f13155d21738383701
hannesgerstmayr <hannes.gerstmayr@gmail.com>
2018-02-09 18:43:46 +0100

    prevented crash when dragging folder in startscreen

--- RawFileCleaner/main.js
+++ RawFileCleaner/main.js
@@ -9,8 +9,8 @@ let mainWindow;
 //Starting App
 app.on('ready', function(){
   mainWindow = new BrowserWindow({
-    width: 800,
-    height: 400,
+    width: 900,
+    height: 450,
     resizable: false,
     fullscreen: false,
     titleBarStyle: 'hiddenInset',
