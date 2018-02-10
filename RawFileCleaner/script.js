5b1fdd180a601b42e6f853aee2026dbec1261e06
Christian Straßmayr <christian.strassmayr@gmail.com>
2018-02-10 11:39:44 +0100

    Worked on script

--- RawFileCleaner/script.js
+++ RawFileCleaner/script.js
@@ -3,6 +3,7 @@ var electronFs = remote.require('fs');
 const trash = require('trash');
 storage = require('electron-json-storage');
 var includeSubfolders = false;
+var filePath;
 
 var allRawFormats = ["K25", "RAW", "NRW", "CR2", "ARW", "RAF", "RWZ", "NEF", "FFF", "DNG", "DCR", "RW2", "3FR", "CRW", "ARI", "ORF",
     "SRF", "MOS", "BAY", "MFW", "EIP", "KDC", "SRW", "MEF", "MRW", "ERF", "J6I", "SR2", "X3F", "RWL", "PEF", "IIQ", "CXI", "CS1"
@@ -24,24 +25,12 @@ function includeSubfolder() {
 }
 
 function cleanFiles() {
-    console.log(allCompressedFiles.length);
+    storage.get('path', function(error, path) {
+        if (error) throw error;
+        filePath = path;
+        console.log(path);
+    });
     getAllFileNames();
-<<<<<<< HEAD
-    console.log(allCompressedFiles.length);
-    while (allCompressedFiles.length != 0) {
-        deleteExistingNames();
-    }
-    console.log("testafterwhile");
-}
-
-function deleteExistingNames() {
-    for (var i = 0; i < allCompressedFiles.length; i++) {
-        for (var j = 0; i < allRawFiles.length; j++) {
-            if (allRawFiles[j] === allCompressedFiles[i]) {
-                allRawFiles.splice(j, 1);
-                allCompressedFiles.splice(i, 1);
-                return;
-=======
     printAllFileNames();
     filterFilesWithoutMatch();
 }
@@ -50,10 +39,9 @@ function filterFilesWithoutMatch() {
     var foundMatch;
     for (var i = 0; i < allRawFiles.length; i++) {
         fountMatch = false;
-        for (var j = 0; i < allCompressedFiles.length && !fountMatch; j++)  {
+        for (var j = 0; i < allCompressedFiles.length && !fountMatch; j++) {
             if (allRawFiles[i].startsWith(allCompressedFiles[j])) {
                 foundMatch = true;
->>>>>>> 6c54e7b3417ed64998f3ca3d33af0cd6a508e567
             }
         }
         if (!foundMatch) {
@@ -62,11 +50,6 @@ function filterFilesWithoutMatch() {
     }
 }
 
-<<<<<<< HEAD
-function getAllFileNames() {
-    console.log("getAll");
-    storage.get('path', function(error, path) {
-=======
 function printAllFileNames() {
     console.log("All Raw" + allRawFiles.length + ": ");
     for (var i = 0; i < allRawFiles.length; i++) {
@@ -82,51 +65,24 @@ function printAllFileNames() {
     }
 }
 
-function getAllFileNames () {
-    storage.get('path', function (error, path) {
->>>>>>> 6c54e7b3417ed64998f3ca3d33af0cd6a508e567
+function getAllFileNames() {
+    storage.get('path', function(error, path) {
         if (error) throw error;
-        console.log("storage get");
         electronFs.readdir(path, (err, dir) => {
-            console.log("readdir");
             for (var i = 0; i < dir.length; i++) {
                 fileName = dir[i];
                 if (endsWithRawFormat(fileName)) {
-<<<<<<< HEAD
-                    console.log("push Raw");
-                    allRawFiles.push(fileName);
-                } else if (endsWithCompressedFormat(fileName)) {
-                    console.log("push Compressed");
-=======
                     console.log("push raw")
                     allRawFiles.push(fileName);
-                }
-                else if (endsWithCompressedFormat(fileName)) {
+                } else if (endsWithCompressedFormat(fileName)) {
                     console.log("push compressed")
->>>>>>> 6c54e7b3417ed64998f3ca3d33af0cd6a508e567
                     allCompressedFiles.push(fileName);
                 }
             }
         });
     });
-    printAllFileNames();
 }
 
-<<<<<<< HEAD
-function printAllFileNames() {
-    console.log("printAll");
-    console.log("All " + allRawFiles.length + ": ");
-    for (var i = 0; i < allRawFiles.length; i++) {
-        console.log(allRawFiles[i]);
-    }
-    console.log("All " + allCompressedFiles.length + ": ");
-    for (var i = 0; i < allCompressedFiles.length; i++) {
-        console.log(allCompressedFiles[i]);
-    }
-}
-
-=======
->>>>>>> 6c54e7b3417ed64998f3ca3d33af0cd6a508e567
 function endsWithRawFormat(fileName) {
     for (var i = 0; i < allRawFormats.length; i++) {
         if (fileName.endsWith(allRawFormats[i])) {
