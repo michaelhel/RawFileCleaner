b23ea8a7a325ef6be7a5f1994b3278a3a8ae3860
michaelhel <michael.helcig@mailbox.org>
2018-06-06 10:22:54 +0200

    Changed color of progressbar

--- RawFileCleaner/script.js
+++ RawFileCleaner/script.js
@@ -174,7 +174,7 @@ function setProgress(progress) {
         strokeWidth: 4,
         easing: 'easeInOut',
         duration: 1400,
-        color: '#FFEA82',
+        color: 'white',
         trailColor: '#eee',
         trailWidth: 1,
         svgStyle: {width: '100%', height: '100%'}
